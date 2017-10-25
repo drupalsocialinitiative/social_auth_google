8b2f51aa4c739bf24bb48568b4278d8ce0091701
Getulio Sánchez <valentin2507@gmail.com>
2017-10-24 21:03:45 -0400

    Issue #2918567: Add functional test for settings form

---
+++ tests/src/Functional/SocialAuthGoogleSettingsFormTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\Tests\social_auth_google\Functional;
+
+use Drupal\social_api\SocialApiSettingsFormBase;
+
+/**
+ * Test Social Auth Google module functionality of settings' forms.
+ *
+ * @group social_auth
+ *
+ * @ingroup social_auth_google
+ */
+class SocialAuthGoogleSettingsFormTest extends SocialApiSettingsFormBase {
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['social_auth_google'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    $this->module = 'social_auth_google';
+    $this->socialNetwork = 'google';
+    $this->moduleType = 'social-auth';
+
+    parent::setUp();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function testIsAvailableInIntegrationList() {
+    $this->fields = ['client_id', 'client_secret'];
+
+    parent::testIsAvailableInIntegrationList();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function testSettingsFormSubmission() {
+    $this->edit = [
+      'client_id' => $this->randomString(10),
+      'client_secret' => $this->randomString(10),
+    ];
+
+    parent::testSettingsFormSubmission();
+  }
+
+}
