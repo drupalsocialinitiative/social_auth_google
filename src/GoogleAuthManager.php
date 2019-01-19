2e828a17aba74c89b8e1121f86a41e298869e506
Getulio Sánchez <valentin2507@gmail.com>
2019-01-18 20:46:32 -0500

    Issue #3027009: Allow developers to specify the HTTP method

--- src/GoogleAuthManager.php
+++ src/GoogleAuthManager.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\social_auth_google;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\social_auth\AuthManager\OAuth2Manager;
 use Drupal\Core\Config\ConfigFactory;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 
 /**
  * Contains all the logic for Google OAuth2 authentication.
@@ -15,24 +17,35 @@ class GoogleAuthManager extends OAuth2Manager {
    *
    * @param \Drupal\Core\Config\ConfigFactory $configFactory
    *   Used for accessing configuration object factory.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger factory.
    */
-  public function __construct(ConfigFactory $configFactory) {
-    parent::__construct($configFactory->get('social_auth_google.settings'));
+  public function __construct(ConfigFactory $configFactory, LoggerChannelFactoryInterface $logger_factory) {
+    parent::__construct($configFactory->get('social_auth_google.settings'), $logger_factory);
   }
 
   /**
    * {@inheritdoc}
    */
   public function authenticate() {
-    $this->setAccessToken($this->client->getAccessToken('authorization_code',
-      ['code' => $_GET['code']]));
+    try {
+      $this->setAccessToken($this->client->getAccessToken('authorization_code',
+        ['code' => $_GET['code']]));
+    }
+    catch (IdentityProviderException $e) {
+      $this->loggerFactory->get('social_auth_google')
+        ->error('There was an error during authentication. Exception: ' . $e->getMessage());
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function getUserInfo() {
-    $this->user = $this->client->getResourceOwner($this->getAccessToken());
+    if (!$this->user) {
+      $this->user = $this->client->getResourceOwner($this->getAccessToken());
+    }
+
     return $this->user;
   }
 
@@ -45,13 +58,13 @@ class GoogleAuthManager extends OAuth2Manager {
       'profile',
     ];
 
-    $google_scopes = $this->getScopes();
-    if ($google_scopes) {
-      if (strpos($google_scopes, ',')) {
-        $scopes = array_merge($scopes, explode(',', $google_scopes));
+    $extra_scopes = $this->getScopes();
+    if ($extra_scopes) {
+      if (strpos($extra_scopes, ',')) {
+        $scopes = array_merge($scopes, explode(',', $extra_scopes));
       }
       else {
-        $scopes[] = $google_scopes;
+        $scopes[] = $extra_scopes;
       }
     }
 
@@ -64,12 +77,24 @@ class GoogleAuthManager extends OAuth2Manager {
   /**
    * {@inheritdoc}
    */
-  public function requestEndPoint($path) {
-    $url = 'https://www.googleapis.com/' . $path;
+  public function requestEndPoint($method, $path, $domain = NULL) {
+    if (!$domain) {
+      $domain = 'https://www.googleapis.com';
+    }
 
-    $request = $this->client->getAuthenticatedRequest('GET', $url, $this->getAccessToken());
+    $url = $domain . $path;
 
-    return $this->client->getParsedResponse($request);
+    $request = $this->client->getAuthenticatedRequest($method, $url, $this->getAccessToken());
+
+    try {
+      return $this->client->getParsedResponse($request);
+    }
+    catch (IdentityProviderException $e) {
+      $this->loggerFactory->get('social_auth_google')
+        ->error('There was an error when requesting ' . $url . '. Exception: ' . $e->getMessage());
+    }
+
+    return NULL;
   }
 
   /**
