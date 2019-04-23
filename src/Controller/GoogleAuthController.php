06c1431657945b9587db267567e0d4f0a75804b1
Getulio Sánchez <valentin2507@gmail.com>
2019-04-22 22:56:15 -0400

    Issue #3050081: Pass renderer service to controller constructor

--- src/Controller/GoogleAuthController.php
+++ src/Controller/GoogleAuthController.php
@@ -3,6 +3,7 @@
 namespace Drupal\social_auth_google\Controller;
 
 use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\social_api\Plugin\NetworkManager;
 use Drupal\social_auth\Controller\OAuth2ControllerBase;
 use Drupal\social_auth\SocialAuthDataHandler;
@@ -31,15 +32,20 @@ class GoogleAuthController extends OAuth2ControllerBase {
    *   Used to access GET parameters.
    * @param \Drupal\social_auth\SocialAuthDataHandler $data_handler
    *   The Social Auth data handler.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   Used to handle metadata for redirection to authentication URL.
    */
   public function __construct(MessengerInterface $messenger,
                               NetworkManager $network_manager,
                               UserAuthenticator $user_authenticator,
                               GoogleAuthManager $google_manager,
                               RequestStack $request,
-                              SocialAuthDataHandler $data_handler) {
+                              SocialAuthDataHandler $data_handler,
+                              RendererInterface $renderer) {
 
-    parent::__construct('Social Auth Google', 'social_auth_google', $messenger, $network_manager, $user_authenticator, $google_manager, $request, $data_handler);
+    parent::__construct('Social Auth Google', 'social_auth_google',
+                        $messenger, $network_manager, $user_authenticator,
+                        $google_manager, $request, $data_handler, $renderer);
   }
 
   /**
@@ -52,7 +58,8 @@ class GoogleAuthController extends OAuth2ControllerBase {
       $container->get('social_auth.user_authenticator'),
       $container->get('social_auth_google.manager'),
       $container->get('request_stack'),
-      $container->get('social_auth.data_handler')
+      $container->get('social_auth.data_handler'),
+      $container->get('renderer')
     );
   }
 
